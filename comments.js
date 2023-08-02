58a3bb99a4a7528d21d4f7f9368220f6928ff43f
Manjari Ghosh <manjighosh@gmail.com>
2023-08-02 12:00:08 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// Create web server application
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Create application/json parser
+var jsonParser = bodyParser.json();
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Create application/json parser
+var jsonParser = bodyParser.json();
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Create application/json parser
+var jsonParser = bodyParser.json();
